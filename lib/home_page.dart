547f73f34be5e2da95e2e71775996c78c8306362
Aman Rajan Singh <madhusingh74055@gmail.com>
2022-04-26 17:45:11 +0530

    2nd commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Catelog App"),
+        ),
+        body: Center(
+          child: Container(
+            child: const Text("Welcome to 30 days Flutter App Development."),
+          ),
+        ),
+        drawer: const Drawer(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  int days = 30;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Flutter App Development."),
-          ),
-        ),
-      ),
-    );
+    return HomePage();
   }
 }
